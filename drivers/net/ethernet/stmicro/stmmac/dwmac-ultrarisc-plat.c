e7013c6b990fe10b4d1420704096e54131080c08
luoming <luoming@ultrarisc.com>
2023-02-06 13:22:52 +0800

    Add ultrarisc DWMAC, probe success

---
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-ultrarisc-plat.c
@@ -0,0 +1,153 @@
+// SPDX-License-Identifier: GPL-2.0
+/* UltraRisc DWMAC platform driver
+ *
+ * Copyright(C) 2023 UltraRisc Technology Co., Ltd.
+ * 
+ *  Author:  luoming <luoming@ultrarisc.com>
+ */
+
+#include <linux/of_device.h>
+#include "stmmac_platform.h"
+
+struct ultrarisc_dwmac {
+	struct device *dev;
+	void __iomem *regs;
+	struct clk *gmac_clk;
+	struct clk *gmac_clk_tx;
+	struct clk *gmac_clk_rx;
+};
+
+static void ultrarisc_eth_fix_mac_speed(void *priv, unsigned int speed)
+{
+	struct ultrarisc_dwmac *dwmac = priv;
+	unsigned long rate;
+	int err;
+
+	switch (speed) {
+	case SPEED_1000:
+		rate = 125000000;
+		break;
+	case SPEED_100:
+		rate = 25000000;
+		break;
+	case SPEED_10:
+		rate = 2500000;
+		break;
+	default:
+		dev_err(dwmac->dev, "invalid speed %u\n", speed);
+		return;
+	}
+
+	err = clk_set_rate(dwmac->gmac_clk_tx, rate);
+	if (err < 0)
+		dev_err(dwmac->dev, "failed to set tx rate %lu\n", rate);
+}
+
+static const struct of_device_id ultrarisc_eth_plat_match[] = {
+	{.compatible = "ultrarisc,dwmac"},
+	{ }
+};
+
+static int ultrarisc_eth_plat_probe(struct platform_device *pdev)
+{
+	struct device *dev = &pdev->dev;
+	struct plat_stmmacenet_data *plat_dat;
+	struct stmmac_resources stmmac_res;
+	struct ultrarisc_dwmac *dwmac;
+	int err;
+
+	err = stmmac_get_platform_resources(pdev, &stmmac_res);
+	if (err)
+		return err;
+
+	plat_dat = stmmac_probe_config_dt(pdev, stmmac_res.mac);
+	if (IS_ERR(plat_dat)) {
+		dev_err(&pdev->dev, "dt configuration failed\n");
+		return PTR_ERR(plat_dat);
+	}
+
+	dwmac = devm_kzalloc(&pdev->dev, sizeof(*dwmac), GFP_KERNEL);
+	if (!dwmac)
+		return -ENOMEM;
+
+	dwmac->dev = &pdev->dev;
+	dwmac->regs = stmmac_res.addr;
+
+	if (!is_of_node(dev->fwnode))
+		goto bypass_clk_reset_gpio;
+
+	dwmac->gmac_clk = devm_clk_get(&pdev->dev, "gmac-clk");
+	if (IS_ERR(dwmac->gmac_clk)) {
+		err = PTR_ERR(dwmac->gmac_clk);
+		goto err;
+	}
+
+	err = clk_prepare_enable(dwmac->gmac_clk);
+	if (err < 0)
+		goto err;
+
+	dwmac->gmac_clk_tx = devm_clk_get(&pdev->dev, "gmac-clk-tx");
+	if (IS_ERR(dwmac->gmac_clk_tx)) {
+		err = PTR_ERR(dwmac->gmac_clk_tx);
+		goto disable_tx;
+	}
+
+	err = clk_prepare_enable(dwmac->gmac_clk_tx);
+	if (err < 0)
+		goto disable_tx;
+
+	dwmac->gmac_clk_rx = devm_clk_get(&pdev->dev, "gmac-clk-rx");
+	if (IS_ERR(dwmac->gmac_clk_rx)) {
+		err = PTR_ERR(dwmac->gmac_clk_rx);
+		goto disable_rx;
+	}
+
+	err = clk_prepare_enable(dwmac->gmac_clk_rx);
+	if (err < 0)
+		goto disable_rx;
+
+	err = stmmac_dvr_probe(&pdev->dev, plat_dat, &stmmac_res);
+	if (err)
+		goto err;
+
+bypass_clk_reset_gpio:
+	plat_dat->fix_mac_speed = ultrarisc_eth_fix_mac_speed;
+	plat_dat->init = NULL;
+	plat_dat->bsp_priv = dwmac;
+	return 0;
+
+disable_rx:
+	clk_disable_unprepare(dwmac->gmac_clk_rx);
+disable_tx:
+	clk_disable_unprepare(dwmac->gmac_clk_tx);
+err:
+	stmmac_remove_config_dt(pdev, plat_dat);
+	return err;
+}
+
+static int ultrarisc_eth_plat_remove(struct platform_device *pdev)
+{
+	struct ultrarisc_dwmac *dwmac = get_stmmac_bsp_priv(&pdev->dev);
+
+	clk_disable_unprepare(dwmac->gmac_clk_rx);
+	clk_disable_unprepare(dwmac->gmac_clk_tx);
+	clk_disable_unprepare(dwmac->gmac_clk);
+
+	return 0;
+}
+
+static struct platform_driver ultrarisc_eth_plat_driver = {
+	.probe  = ultrarisc_eth_plat_probe,
+	.remove = ultrarisc_eth_plat_remove,
+	.driver = {
+		.name = "ultrarisc-eth-plat",
+		.pm = &stmmac_pltfr_pm_ops,
+		.of_match_table = ultrarisc_eth_plat_match,
+	},
+};
+
+module_platform_driver(ultrarisc_eth_plat_driver);
+
+MODULE_LICENSE("GPL v2");
+MODULE_DESCRIPTION("UltraRisc DWMAC platform driver");
+
